c73930013f95d1778b6c91f1b3ab1f35734eff07
manhattan <rhdevane@gmail.com>
2020-09-26 22:09:12 -0400

    made generic fields

--- plot_params.py
+++ plot_params.py
@@ -18,16 +18,16 @@ xtick_fontsize = 14
 ytick_fontsize=12
 ylabel_fontsize=14
 # axis field:[line type,color]
-plot_fields = {'ROAS':['bar', 'slategray'],
-                'Clicks':['line', 'orangered'],
-                'Spend':['line', 'royalblue'],
-                'Sales':['line', 'black'],
-                'Impressions':['line', 'darkcyan']}
+plot_fields = {'Field1':['bar', 'slategray'],
+                'Field2':['line', 'orangered'],
+                'Field3':['line', 'royalblue'],
+                'Field4':['line', 'black'],
+                'Field5':['line', 'darkcyan']}
 
 # info for bar chart text
 onbar_fontsize=14
 onbar_fontcolor = 'darkblue'
-on_bar_field_1 = 'Conv_Rate'
-on_bar_label_1 = 'CR'
-on_bar_field_2 = 'CPC'
-on_bar_label_2 = 'CPC'
+on_bar_field_1 = 'Field5'
+on_bar_label_1 = 'Field5'
+on_bar_field_2 = 'Field6'
+on_bar_label_2 = 'Field6'
